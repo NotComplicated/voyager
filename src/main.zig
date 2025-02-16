9f3dedc01884aa2d476aadcdf45a22834d87248b
Andres <andresdejesus123@gmail.com>
2025-02-16 00:33:28 -0600

    better memory management, catppuccin theme

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,19 @@
 const builtin = @import("builtin");
+const debug = builtin.mode == .Debug;
 
 const std = @import("std");
 const heap = std.heap;
 const mem = std.mem;
 const enums = std.enums;
-
-const GPA = heap.GeneralPurposeAllocator(.{});
+const fs = std.fs;
 
 const clay = @import("clay");
 const renderer = clay.renderers.raylib;
 
 const rl = @import("raylib");
 
-const title = "Voyager";
-const width = 800;
+const title = "Voyager" ++ if (debug) " (Debug)" else "";
+const width = if (debug) 1200 else 800;
 const height = 480;
 
 const rl_config = rl.ConfigFlags{
@@ -35,17 +35,25 @@ fn rgb(r: u8, g: u8, b: u8) clay.Color {
     return .{ .r = @floatFromInt(r), .g = @floatFromInt(g), .b = @floatFromInt(b) };
 }
 
+const catppuccin = .{
+    .text = rgb(205, 214, 244),
+    .base = rgb(30, 30, 46),
+    .mantle = rgb(24, 24, 37),
+};
+
 fn vector_conv(v: rl.Vector2) clay.Vector2 {
     return .{ .x = v.x, .y = v.y };
 }
 
-const black = rgb(0, 0, 0);
-const white = rgb(255, 255, 255);
-
-fn text(contents: []const u8, comptime font_size: FontSize, color: clay.Color) void {
+fn text(contents: []const u8, comptime font_size: FontSize) void {
     inline for (comptime enums.values(FontSize), 0..) |size, id| {
         if (size == font_size) {
-            clay.text(contents, .{ .font_id = id, .font_size = @intFromEnum(size), .color = color });
+            clay.text(contents, .{
+                .color = catppuccin.text,
+                .font_id = id,
+                .font_size = @intFromEnum(size),
+                .wrap_mode = .none,
+            });
             return;
         }
     }
@@ -53,16 +61,12 @@ fn text(contents: []const u8, comptime font_size: FontSize, color: clay.Color) v
 }
 
 pub fn main() !void {
-    var gpa = GPA{};
-    defer _ = gpa.deinit();
-    const alloc = gpa.allocator();
-
-    const arena = clay.createArena(alloc, clay.minMemorySize());
-    defer alloc.free(@as([*]u8, @ptrCast(arena.memory))[0..arena.capacity]);
+    const arena = clay.createArena(heap.page_allocator, clay.minMemorySize());
+    defer heap.page_allocator.free(@as([*]u8, @ptrCast(arena.memory))[0..arena.capacity]);
 
     clay.setMeasureTextFunction(renderer.measureText);
     _ = clay.initialize(arena, .{ .width = width, .height = height }, .{});
-    clay.setDebugModeEnabled(builtin.mode == .Debug);
+    clay.setDebugModeEnabled(debug);
     renderer.initialize(width, height, title, rl_config);
     rl.setExitKey(.null);
 
@@ -73,26 +77,36 @@ pub fn main() !void {
     }
     defer inline for (0..comptime enums.values(FontSize).len) |id| renderer.getFont(id).unload();
 
-    while (!rl.windowShouldClose()) frame(alloc);
+    while (!rl.windowShouldClose()) frame();
 }
 
-fn frame(alloc: mem.Allocator) void {
+fn frame() void {
     const delta = rl.getFrameTime();
-
     clay.setLayoutDimensions(.{ .width = @floatFromInt(rl.getScreenWidth()), .height = @floatFromInt(rl.getScreenHeight()) });
     clay.setPointerState(vector_conv(rl.getMousePosition()), rl.isMouseButtonDown(.left));
     clay.updateScrollContainers(true, vector_conv(rl.getMouseWheelMoveV()), delta);
-    rl.clearBackground(rl.Color.light_gray);
 
-    clay.beginLayout();
     rl.beginDrawing();
     defer rl.endDrawing();
-    defer renderer.render(clay.endLayout(), alloc);
+
+    clay.beginLayout();
+    defer {
+        // a buffer for the raylib renderer to use for temporary string copies
+        var buf: [fs.max_path_bytes + 1]u8 = undefined;
+        var fba = heap.FixedBufferAllocator.init(&buf);
+        renderer.render(clay.endLayout(), fba.allocator());
+    }
 
     clay.ui()(.{
-        .id = clay.idi("Foo", 0),
-        .layout = .{ .padding = clay.Padding.all(5) },
+        .id = clay.id("Screen"),
+        .layout = .{
+            .sizing = .{
+                .width = .{ .size = .{ .percent = 1 }, .type = .percent },
+                .height = .{ .size = .{ .percent = 1 }, .type = .percent },
+            },
+        },
+        .rectangle = .{ .color = catppuccin.base },
     })({
-        text("Hello, world!", .md, black);
+        //
     });
 }
